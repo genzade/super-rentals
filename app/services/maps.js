f8ee0d99870de6518ece9b3b265888c8de3eee9a
HamidMohammadZadeh <zade@HamidMohammadZadehs-MacBook-Pro.local>
2016-08-21 20:31:15 +0100

    adds google maps location services to app

---
+++ app/services/maps.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import MapUtil from '../utils/google-maps';
+
+export default Ember.Service.extend({
+
+  init() {
+    if (!this.get('cashedMaps')) {
+      this.set('cashedMaps', Ember.Object.create());
+    }
+    if (!this.get('mapUtil')) {
+      this.set('mapUtil', MapUtil.create());
+    }
+  },
+
+  getMapElement(location) {
+    let camelizedLocation = location.camelize();
+    let element = this.get(`cashedMaps.${camelizedLocation}`);
+    if (!element) {
+      element = this.createMapElement();
+      this.get('mapUtil').createMap(element, location);
+      this.set(`cashedMaps.${camelizedLocation}`, element);
+    }
+    return element;
+  },
+
+  createMapElement() {
+    let element = document.createElement('div');
+    element.className = 'map';
+    return element;
+  }
+});
